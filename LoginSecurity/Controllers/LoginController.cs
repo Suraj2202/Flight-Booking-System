5bea9de18b5442e8749faaf6bf9de410d7ca8335
gulusuraj <gulusurajverma@gmail.com>
2022-05-15 16:02:57 +0530

    initial

---
+++ LoginSecurity/Controllers/LoginController.cs
@@ -0,0 +1,74 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace LoginSecurity.Controllers
+{
+
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LoginController : ControllerBase
+    {
+
+        private string GenerateJsonWebToken(string username)
+        {
+            var securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("Flight@Booking@1"));
+            var credentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
+            var claims = new[]
+            {
+                new Claim("Issuer", "Admin_Suraj"),
+                new Claim("Admin", "true"),
+                new Claim(JwtRegisteredClaimNames.UniqueName, username)
+            };
+            var token = new JwtSecurityToken(
+                "Admin_Suraj",
+                "Client",
+                claims,
+                expires: DateTime.Now.AddMinutes(120),
+                signingCredentials: credentials);
+
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
+
+        // GET: api/<LoginController>
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "Login Successfull redirecting to Booking Page" };
+        }
+
+        // GET api/<LoginController>/5
+        [HttpGet("{uname}")]
+        public string Get(string uname)
+        {
+            return GenerateJsonWebToken(uname);
+        }
+
+        // POST api/<LoginController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<LoginController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<LoginController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ Modify_Airline_Details/Controllers/DataModificationController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Modify_Airline_Details.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DataModificationController : ControllerBase
+    {
+        // GET: api/<DataModificationController>
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "Data Modification operations like Add, Block, cancel Airlines" };
+        }
+
+        // GET api/<DataModificationController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<DataModificationController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<DataModificationController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<DataModificationController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
